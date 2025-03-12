95a2383bb27bf755f638dddcf507dd35617e44c9
Mu Xianming <mu.xianming@lmwn.com>
2025-03-12 16:41:58 +0800

    ex1

--- interp-Lint.rkt
+++ interp-Lint.rkt
@@ -71,5 +71,39 @@
         ))
     ))
 
+(define (pe_neg r)
+  (match r
+    [(Int n) (Int (fx- 0 n))]
+    [else (Prim '- (list r))]))
 
+(define (pe_add r1 r2)
+  (match* (r1 r2)
+    [((Int n1) (Int n2)) (Int (fx+ n1 n2))]
+    [(_ _) (Prim '+ (list r1 r1))]))
+
+(define (pe_sub r1 r2)
+  (match* (r1 r2)
+    [((Int n1) (Int n2)) (Int (fx- n1 n2))]
+    [(_ _) (Prim '- (list r1 r2))]))
+
+(define (pe_exp e)
+  (match e
+    [(Int n) (Int n)]
+    [(Prim 'read '()) (Prim 'read '())]
+    [(Prim '- (list e1)) (pe_neg (pe_exp e1))]
+    [(Prim '+ (list e1 e2)) (pe_add (pe_exp e1) (pe_exp e2))]
+    [(Prim '- (list e1 e2)) (pe_sub (pe_exp e1) (pe_exp e2))]))
+
+(define (pe_Lint p)
+  (match p
+    [(Program '() e) (Program '() (pe_exp e))]))
+
+
+(define (test_pe p)
+  (assert "testing pe_Lint"
+    (equal? (interp-Lint p) (interp-Lint (pe_Lint p)))))
+
+
+(test_pe (parse-program `(program () (+ 10 (- (+ 5 3))))))
+(test_pe (parse-program `(program () (+ 10 (read)))))
 
